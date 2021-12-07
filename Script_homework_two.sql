a3dc69b9be8015b90fd5df7023f6dfcd7749ccc4
AlexanderIon <tov.sandro@mail.ru>
2021-12-07 12:20:15 +0300

    устранение замечаний

--- Script_homework_two.sql
+++ Script_homework_two.sql
@@ -6,16 +6,18 @@ create table if not exists Genars (
 									name_genar varchar(50) not null unique );
 
 create table if not exists Singer_and_Genar(
+											id_sin_and_genar primary key (id_singer, id_genar),			
 											id_singer integer references Singers (id_singer),
 											id_genar integer references Genars (id_genar));									
 
 create table if not exists Albums (
 									id_album serial primary key not null,
-									id_singer integer references Singers(id_singer),
+									
 									name_album varchar(80) not null ,
 									year_create integer not null);
 
 create table if not exists Album_and_Singer (
+											 id_album_and_singer primary key (id_album,id_singer),	
 											 id_album integer references Albums(id_album),
 											 id_singer integer references Singers(id_singer));
 											
@@ -31,10 +33,11 @@ create table if not exists Tune (
 								 lenght_tune integer not null);
 								
 create table if not exists Tune_in_Collection (
+											   id_tune_and_collect primary key (id_tune,id_collection),	
 											   id_tune integer references Tune (id_tune),
 											   id_collection integer references Collection (id_collection));
 											  
-									
+								
 
 
 
